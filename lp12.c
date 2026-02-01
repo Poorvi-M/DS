2bf3439ea0eb54fd75b1421dc55551db1a33bb45
Poorvi <poorvimanu08@gmail.com>
2026-02-01 18:13:27 +0530

    Refactor queue and deque implementation

--- lp12.c
+++ lp12.c
@@ -1,296 +1,320 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int *q, front = -1, rear = -1, size;
-
-// void enqueue(int x)
-// {
-//     if(rear == size - 1) return;
-//     if(front == -1) front = 0;
-//     q[++rear] = x;
-// }
-
-// void dequeue()
-// {
-//     if(front == -1) return;
-//     front++;
-//     if(front > rear) front = rear = -1;
-// }
-
-// void display()
-// {
-//     if(front == -1) return;
-//     for(int i = front; i <= rear; i++)
-//         printf("%d ", q[i]);
-// }
-
-// int main()
-// {
-//     int choice, x;
-
-//     printf("Enter queue size: ");
-//     scanf("%d", &size);
-//     q = malloc(size * sizeof(int));
-
-//     do
-//     {
-//         printf("\nLP-12 MENU");
-//         printf("\n1.Enqueue");
-//         printf("\n2.Dequeue");
-//         printf("\n3.Display");
-//         printf("\n0.Exit");
-//         scanf("%d", &choice);
-
-//         switch(choice)
-//         {
-//             case 1: scanf("%d", &x); enqueue(x); break;
-//             case 2: dequeue(); break;
-//             case 3: display(); break;
-//         }
-//     } while(choice != 0);
-
-//     free(q);
-//     return 0;
-// }
 #include <stdio.h>
 #include <stdlib.h>
 
-/* ================= ORDINARY QUEUE ================= */
-
-struct Queue {
+typedef struct {
     int *arr;
-    int front, rear, size;
-};
+    int capacity;
+    int size;
+    int front, rear;
+} Queue;
 
-void initQueue(struct Queue *q, int size) {
-    q->size = size;
-    q->front = q->rear = -1;
-    q->arr = (int *)malloc(size * sizeof(int));
+/* Function prototypes */
+void initQueue(Queue *q);
+int isEmpty(Queue *q);
+int isFull(Queue *q);
+void resize(Queue *q);
+void enqueue(Queue *q, int item);
+void dequeue(Queue *q);
+void peekFront(Queue *q);
+void peekRear(Queue *q);
+void display(Queue *q);
+
+/* MAIN */
+int main()
+{
+    Queue q;
+    int choice, val;
+
+    initQueue(&q);
+
+    do {
+        printf("\n--- ORDINARY QUEUE MENU ---");
+        printf("\n1. Enqueue");
+        printf("\n2. Dequeue");
+        printf("\n3. Peek Front");
+        printf("\n4. Peek Rear");
+        printf("\n5. Display");
+        printf("\n0. Exit");
+        printf("\nEnter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                enqueue(&q, val);
+                break;
+            case 2:
+                dequeue(&q);
+                break;
+            case 3:
+                peekFront(&q);
+                break;
+            case 4:
+                peekRear(&q);
+                break;
+            case 5:
+                display(&q);
+                break;
+        }
+    } while(choice != 0);
+
+    free(q.arr);
+    return 0;
 }
 
-int isFull(struct Queue *q) {
-    return (q->rear == q->size - 1);
+/* INITIALIZE */
+void initQueue(Queue *q)
+{
+    q->capacity = 4;
+    q->size = 0;
+    q->front = 0;
+    q->rear = -1;
+    q->arr = (int*)malloc(q->capacity * sizeof(int));
 }
 
-int isEmpty(struct Queue *q) {
-    return (q->front == -1 || q->front > q->rear);
+int isEmpty(Queue *q)
+{
+    return q->size == 0;
 }
 
-void enqueue(struct Queue *q, int val) {
-    if (isFull(q)) {
-        printf("Queue Overflow\n");
-        return;
-    }
-    if (q->front == -1)
-        q->front = 0;
-
-    q->arr[++q->rear] = val;
-    printf("Inserted %d\n", val);
+int isFull(Queue *q)
+{
+    return q->size == q->capacity;
 }
 
-void dequeue(struct Queue *q) {
-    if (isEmpty(q)) {
+void resize(Queue *q)
+{
+    q->capacity *= 2;
+    q->arr = (int*)realloc(q->arr, q->capacity * sizeof(int));
+    printf("Allocating more memory...\n");
+}
+
+/* ENQUEUE */
+void enqueue(Queue *q, int item)
+{
+    if(isFull(q))
+        resize(q);
+
+    q->arr[++q->rear] = item;
+    q->size++;
+}
+
+/* DEQUEUE */
+void dequeue(Queue *q)
+{
+    if(isEmpty(q)) {
         printf("Queue Underflow\n");
         return;
     }
 
-    printf("Deleted %d\n", q->arr[q->front++]);
+    printf("Deleted %d\n", q->arr[q->front]);
 
-    if (q->front > q->rear)
-        q->front = q->rear = -1;
+    for(int i = 0; i < q->rear; i++)
+        q->arr[i] = q->arr[i + 1];
+
+    q->rear--;
+    q->size--;
 }
 
-void displayQueue(struct Queue *q) {
-    if (isEmpty(q)) {
+/* PEEK FRONT */
+void peekFront(Queue *q)
+{
+    if(isEmpty(q)) {
+        printf("Queue Empty\n");
+        return;
+    }
+    printf("Front element: %d\n", q->arr[q->front]);
+}
+
+/* PEEK REAR */
+void peekRear(Queue *q)
+{
+    if(isEmpty(q)) {
+        printf("Queue Empty\n");
+        return;
+    }
+    printf("Rear element: %d\n", q->arr[q->rear]);
+}
+
+/* DISPLAY */
+void display(Queue *q)
+{
+    if(isEmpty(q)) {
         printf("Queue Empty\n");
         return;
     }
 
     printf("Queue: ");
-    for (int i = q->front; i <= q->rear; i++)
+    for(int i = 0; i <= q->rear; i++)
         printf("%d ", q->arr[i]);
     printf("\n");
 }
+#include <stdio.h>
+#include <stdlib.h>
 
-/* ================= DOUBLE ENDED QUEUE ================= */
-
-struct Deque {
+typedef struct {
     int *arr;
-    int front, rear, size;
-};
+    int capacity;
+    int front, rear;
+} Deque;
 
-void initDeque(struct Deque *dq, int size) {
-    dq->size = size;
-    dq->front = dq->rear = -1;
-    dq->arr = (int *)malloc(size * sizeof(int));
+/* Function prototypes */
+void initDeque(Deque *dq);
+int isEmpty(Deque *dq);
+int isFull(Deque *dq);
+void resize(Deque *dq);
+void insertFront(Deque *dq, int item);
+void insertRear(Deque *dq, int item);
+void deleteFront(Deque *dq);
+void deleteRear(Deque *dq);
+void display(Deque *dq);
+
+/* MAIN */
+int main()
+{
+    Deque dq;
+    int choice, val;
+
+    initDeque(&dq);
+
+    do {
+        printf("\n--- DEQUE MENU ---");
+        printf("\n1. Insert Front");
+        printf("\n2. Insert Rear");
+        printf("\n3. Delete Front");
+        printf("\n4. Delete Rear");
+        printf("\n5. Display");
+        printf("\n0. Exit");
+        printf("\nEnter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                insertFront(&dq, val);
+                break;
+            case 2:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                insertRear(&dq, val);
+                break;
+            case 3:
+                deleteFront(&dq);
+                break;
+            case 4:
+                deleteRear(&dq);
+                break;
+            case 5:
+                display(&dq);
+                break;
+        }
+    } while(choice != 0);
+
+    free(dq.arr);
+    return 0;
 }
 
-int isDEmpty(struct Deque *dq) {
-    return (dq->front == -1);
+/* INITIALIZE */
+void initDeque(Deque *dq)
+{
+    dq->capacity = 4;
+    dq->front = -1;
+    dq->rear = 0;
+    dq->arr = (int*)malloc(dq->capacity * sizeof(int));
 }
 
-int isDFull(struct Deque *dq) {
-    return (dq->front == 0 && dq->rear == dq->size - 1);
+int isEmpty(Deque *dq)
+{
+    return dq->front == -1;
 }
 
-void insertFront(struct Deque *dq, int val) {
-    if (isDFull(dq)) {
-        printf("Deque Overflow\n");
-        return;
-    }
+int isFull(Deque *dq)
+{
+    return (dq->front == 0 && dq->rear == dq->capacity - 1);
+}
 
-    if (dq->front == -1)
+/* RESIZE */
+void resize(Deque *dq)
+{
+    dq->capacity *= 2;
+    dq->arr = (int*)realloc(dq->arr, dq->capacity * sizeof(int));
+    printf("Allocating more memory...\n");
+}
+
+/* INSERT FRONT */
+void insertFront(Deque *dq, int item)
+{
+    if(isFull(dq))
+        resize(dq);
+
+    if(isEmpty(dq)) {
         dq->front = dq->rear = 0;
-    else
-        dq->front--;
-
-    dq->arr[dq->front] = val;
-    printf("Inserted %d at front\n", val);
+    } else {
+        for(int i = dq->rear; i >= dq->front; i--)
+            dq->arr[i + 1] = dq->arr[i];
+        dq->rear++;
+    }
+    dq->arr[dq->front] = item;
 }
 
-void insertRear(struct Deque *dq, int val) {
-    if (isDFull(dq)) {
-        printf("Deque Overflow\n");
-        return;
-    }
+/* INSERT REAR */
+void insertRear(Deque *dq, int item)
+{
+    if(isFull(dq))
+        resize(dq);
 
-    if (dq->rear == -1)
+    if(isEmpty(dq))
         dq->front = dq->rear = 0;
     else
         dq->rear++;
 
-    dq->arr[dq->rear] = val;
-    printf("Inserted %d at rear\n", val);
+    dq->arr[dq->rear] = item;
 }
 
-void deleteFront(struct Deque *dq) {
-    if (isDEmpty(dq)) {
-        printf("Deque Underflow\n");
+/* DELETE FRONT */
+void deleteFront(Deque *dq)
+{
+    if(isEmpty(dq)) {
+        printf("Deque Empty\n");
         return;
     }
 
-    printf("Deleted %d from front\n", dq->arr[dq->front]);
+    printf("Deleted %d\n", dq->arr[dq->front]);
 
-    if (dq->front == dq->rear)
-        dq->front = dq->rear = -1;
+    if(dq->front == dq->rear)
+        dq->front = -1;
     else
         dq->front++;
 }
 
-void deleteRear(struct Deque *dq) {
-    if (isDEmpty(dq)) {
-        printf("Deque Underflow\n");
+/* DELETE REAR */
+void deleteRear(Deque *dq)
+{
+    if(isEmpty(dq)) {
+        printf("Deque Empty\n");
         return;
     }
 
-    printf("Deleted %d from rear\n", dq->arr[dq->rear]);
+    printf("Deleted %d\n", dq->arr[dq->rear]);
 
-    if (dq->front == dq->rear)
-        dq->front = dq->rear = -1;
+    if(dq->front == dq->rear)
+        dq->front = -1;
     else
         dq->rear--;
 }
 
-void displayDeque(struct Deque *dq) {
-    if (isDEmpty(dq)) {
+/* DISPLAY */
+void display(Deque *dq)
+{
+    if(isEmpty(dq)) {
         printf("Deque Empty\n");
         return;
     }
 
     printf("Deque: ");
-    for (int i = dq->front; i <= dq->rear; i++)
+    for(int i = dq->front; i <= dq->rear; i++)
         printf("%d ", dq->arr[i]);
     printf("\n");
-}
-
-/* ================= MAIN MENU ================= */
-
-int main() {
-    struct Queue q;
-    struct Deque dq;
-    int choice, sub, val, size;
-
-    printf("Enter queue size: ");
-    scanf("%d", &size);
-
-    initQueue(&q, size);
-    initDeque(&dq, size);
-
-    do {
-        printf("\n===== MAIN MENU =====");
-        printf("\n1. Ordinary Queue");
-        printf("\n2. Double Ended Queue");
-        printf("\n0. Exit");
-        printf("\nEnter choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-
-        case 1:
-            do {
-                printf("\n--- Ordinary Queue ---");
-                printf("\n1. Enqueue");
-                printf("\n2. Dequeue");
-                printf("\n3. Display");
-                printf("\n0. Back");
-                printf("\nEnter choice: ");
-                scanf("%d", &sub);
-
-                switch (sub) {
-                case 1:
-                    printf("Enter value: ");
-                    scanf("%d", &val);
-                    enqueue(&q, val);
-                    break;
-                case 2:
-                    dequeue(&q);
-                    break;
-                case 3:
-                    displayQueue(&q);
-                    break;
-                }
-            } while (sub != 0);
-            break;
-
-        case 2:
-            do {
-                printf("\n--- Deque ---");
-                printf("\n1. Insert Front");
-                printf("\n2. Insert Rear");
-                printf("\n3. Delete Front");
-                printf("\n4. Delete Rear");
-                printf("\n5. Display");
-                printf("\n0. Back");
-                printf("\nEnter choice: ");
-                scanf("%d", &sub);
-
-                switch (sub) {
-                case 1:
-                    printf("Enter value: ");
-                    scanf("%d", &val);
-                    insertFront(&dq, val);
-                    break;
-                case 2:
-                    printf("Enter value: ");
-                    scanf("%d", &val);
-                    insertRear(&dq, val);
-                    break;
-                case 3:
-                    deleteFront(&dq);
-                    break;
-                case 4:
-                    deleteRear(&dq);
-                    break;
-                case 5:
-                    displayDeque(&dq);
-                    break;
-                }
-            } while (sub != 0);
-            break;
-        }
-    } while (choice != 0);
-
-    free(q.arr);
-    free(dq.arr);
-    return 0;
 }
